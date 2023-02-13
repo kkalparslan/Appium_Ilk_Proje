f2797d1818d49b5c6f08a6f83087e3086d636c0f
alparslan <alparslanztk@gmail.com>
2023-02-13 13:05:48 +0300

    Added new codes about OPPO and 5554 phone in Test1 class

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -9,11 +9,36 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Test1 {
 
     @Test
     public void test1() throws MalformedURLException {
+        // Desired capability
+        // hangi cihaz ve hangi uygulamaya bağlanılacağı ayarlanıyor
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability("appium:udid", "NNR6R20618007244");
+        capabilities.setCapability("appium:version", "12");
+        capabilities.setCapability("appium:deviceName", "Huawei-ec");
+        capabilities.setCapability("platformName", "Android");
+
+        capabilities.setCapability("appium:appPackage", "com.touchboarder.android.api.demos");
+        capabilities.setCapability("appium:appActivity", "com.touchboarder.androidapidemos.MainActivity");
+
+        AppiumDriver<MobileElement>driver;
+        driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        // AppiumJavaClient 8.x.x sürümleri ile birlikte driver tanımı url için /wd/hub kısmı da kaldırılmış.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("com.android.permissioncontroller:id/continue_button")).click();
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElement(By.id("com.touchboarder.android.api.demos:id/buttonDefaultPositive")).click();
+        driver.findElement(By.xpath("//*[@text='API Demos']")).click();
+        driver.closeApp();
+    }
+
+    @Test
+    public void test2() throws MalformedURLException {
         // Desired capability
         // hangi cihaz ve hangi uygulamaya bağlanılacağı ayarlanıyor
         DesiredCapabilities capabilities=new DesiredCapabilities();
@@ -23,12 +48,28 @@ public class Test1 {
         capabilities.setCapability("platformName", "Android");
 
         capabilities.setCapability("appium:appPackage", "com.touchboarder.android.api.demos");
-        capabilities.setCapability("appium:Activity", "com.touchboarder.androidapidemos.MainActivity");
+        capabilities.setCapability("appium:appActivity", "com.touchboarder.androidapidemos.MainActivity");
 
         AppiumDriver<MobileElement>driver;
         driver=new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='API Demos']")).click();
+        // AppiumJavaClient 8.x.x sürümleri ile birlikte driver tanımı url için /wd/hub kısmı da kaldırılmış.
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.id("com.android.permissioncontroller:id/continue_button")).click();
+        driver.findElement(By.id("android:id/button1")).click();
+        driver.findElement(By.id("com.touchboarder.android.api.demos:id/buttonDefaultPositive")).click();
+        driver.findElement(By.xpath("//*[@text='API Demos']")).click();
         driver.closeApp();
-
     }
+    /**
+    driver, dest.
+    driver, cihaz, app
+
+    RemoteWebDriver
+    WebDriver
+                        appium-java-client
+                        7.x.x   8.x.x
+    AppiumDriver        50      5
+        AndroidDriver   5       50
+        IOSDriver       5       50
+ */
 }
